67385117feca9af4f842195893915c37a1916f10
João Costa Seco <jrcs@fct.unl.pt>
2021-12-11 00:20:43 +0000

    Third commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Action, applyMiddleware, createStore, Reducer } from 'redux'
+import { Action, applyMiddleware, combineReducers, createStore, Dispatch, Reducer } from 'redux'
 import { Provider, useDispatch, useSelector } from 'react-redux';
 import React, { useEffect, useState } from 'react';
 import './App.css';
@@ -78,27 +78,63 @@ const reducerBooks: Reducer<RdxBookState> = (state=initialBookState, action:Acti
 }
 
 
-// View components
+// Store for user information
 
-const UserBox = () => <div><p>Mary is logged in</p></div>
-
-const BookList = () => {
-  const books = useSelector( (state:RdxBookState) => state.data )
-  const filter = useSelector( (state:RdxBookState) => state.filter )
-  const loading = useSelector( (state:RdxBookState) => state.loading )
-
-  return loading ? 
-  <p>Is Loading</p>
-  :
-  <ul>
-    {
-      books.filter(b => b.title.includes(filter)).map(b => <li>{b.title}</li>)
-    }
-  </ul>
+interface RdxUserState {
+  username: string | null,
+  roles:string[],
+  waiting:boolean
 }
 
-const useInput: (initialState:string) => [JSX.Element, string, React.Dispatch<React.SetStateAction<string>>] 
-  = 
+const REQUEST_LOGIN = 'REQUEST_LOGIN'
+const requestLogin = () => ({type:REQUEST_LOGIN})
+
+
+const RECEIVE_LOGIN = 'RECEIVE_LOGIN'
+interface ReceiveLoginAction extends Action { username:string, roles:string[] }
+const receiveLogin = (username:string, roles:string[]) => ({type:RECEIVE_LOGIN, username, roles})
+
+
+const LOGOUT = 'LOGOUT'
+const logout = () => ({type:LOGOUT})
+
+
+const remoteLogin = (username:string, password:string) => 
+  (dispatch:Dispatch) => {
+    dispatch(requestLogin())
+    fetch("/login.json") // , {method:"POST", body:JSON.stringify({username,password})})
+    .then( resp => resp.json() )
+    .then( data => dispatch(receiveLogin(data.username,data.roles)))
+  }
+
+
+const initialUserState = {
+  username: null,
+  roles:[],
+  waiting:false
+}
+
+const reducerUsers: Reducer<RdxUserState> = (state = initialUserState, action:Action) => {
+  switch (action.type) {
+    case REQUEST_LOGIN:
+      return {...state, waiting:true }
+  
+    case RECEIVE_LOGIN:
+      const {username,roles} = (action as ReceiveLoginAction)
+      return {...state, waiting:false, username, roles}
+
+    case LOGOUT:
+      return {...state, username:null, roles:[]}
+
+    default:
+      return state
+  }
+}
+
+// Custom hooks
+
+const useInput: (initialState: string) => [JSX.Element, string, React.Dispatch<React.SetStateAction<string>>]
+  =
   (initialState) => {
     const [text, setText] = useState(initialState)
 
@@ -108,6 +144,97 @@ const useInput: (initialState:string) => [JSX.Element, string, React.Dispatch<Re
 
     return [input, text, setText]
   }
+
+
+
+
+// View components for users
+
+const useUserSelector =
+  (fn: (state: RdxUserState) => any) => fn(useSelector((state: RdxGlobalState) => state.users))
+
+const useIsAdmin = () => useUserSelector((state: RdxUserState) => state.roles.includes("ROLE_ADMIN"))
+
+const useIsRegistered = () => useUserSelector((state: RdxUserState) => state.username != null )
+
+const UserInfo = 
+  ({username}:{username:string}) => {
+    const dispatch = useDispatch()
+
+    const handleClick = () => { dispatch(logout())}
+
+    return <p>{username} is logged in <button onClick={handleClick}>Logout</button></p>
+  }
+
+const LoginBox = () => {
+    const dispatch = useDispatch()
+
+    const [inputUsername, username, setUsername] = useInput("")
+    const [inputPassword, password, setPassword] = useInput("")
+
+    const handleSubmit = (e:any) => {
+      console.log("Here")
+      e.preventDefault()
+      dispatch(remoteLogin(username, password))
+      setUsername("")
+      setPassword("")
+    }
+
+    return <form onSubmit={handleSubmit}>
+      <p>Username {inputUsername}</p>
+      <p>Password {inputPassword}</p>
+      <input type="submit" value="Login"/>
+    </form>
+  }
+
+const UserBox = () => {
+  const username = useUserSelector((state: RdxUserState) => state.username )
+
+  return <div>
+        { 
+          username 
+          ? 
+          <UserInfo username={username}/>
+          : 
+          <LoginBox/>
+        } 
+        </div>
+}
+
+
+const AdminArea: React.FunctionComponent<{}> = (props) => {
+  const isAdmin = useIsAdmin()
+
+  return isAdmin ? <>{props.children}</> : <></>
+}
+
+const RegisteredArea: React.FunctionComponent<{}> = (props) => {
+  const isAdmin = useIsRegistered()
+
+  return isAdmin ? <>{props.children}</> : <></>
+}
+
+
+// View components for books
+
+const useBookSelector = 
+  (fn: (state: RdxBookState) => any) => fn(useSelector( (state:RdxGlobalState) => state.books ))
+
+
+const BookList = () => {
+  const books = useBookSelector( (state:RdxBookState) => state.data )
+  const filter = useBookSelector( (state:RdxBookState) => state.filter )
+  const loading = useBookSelector( (state:RdxBookState) => state.loading )
+
+  return loading ? 
+  <p>Is Loading</p>
+  :
+  <ul>
+    {
+      books.filter((b:Book) => b.title.includes(filter)).map((b:Book) => <li>{b.title}</li>)
+    }
+  </ul>
+}
 
 const BookForm = () => {
   const dispatch = useDispatch()
@@ -156,15 +283,22 @@ const Container:React.FunctionComponent<{}> = (props) =>
     {props.children}
   </div>
 
+
+
+
 const PageLayout = () => 
   <div>
     <Header title={"My Personal Library"}>
       <UserBox/>
     </Header>
     <Container>
-      <SearchForm/>
+      <RegisteredArea>
+        <SearchForm/>
+      </RegisteredArea>
       <BookList/>
-      <BookForm/>
+      <AdminArea>
+        <BookForm/>
+      </AdminArea>
     </Container>
   </div>
 
@@ -174,7 +308,13 @@ const PageLayout = () =>
 
 const logger = createLogger()
 
-const store = createStore( reducerBooks, applyMiddleware( thunkMidleware, logger ) )
+interface RdxGlobalState {
+  users: RdxUserState,
+  books: RdxBookState
+}
+const reducer = combineReducers({users:reducerUsers, books: reducerBooks})
+
+const store = createStore(reducer, applyMiddleware( thunkMidleware, logger ) )
 
 const Page = () => {
   const dispatch = useDispatch()
